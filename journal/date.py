d60fe96be303be1d2048c56e08f342d64a5d6fa5
Ted Stein <karamarisan@gmail.com>
2012-07-01 00:00:36 -0500

    Allow relative specifications like "yesterday".

--- journal/date.py
+++ journal/date.py
@@ -7,25 +7,42 @@ from journal.config import getconf
 
 
 Date = namedtuple('Date', ['year', 'month', 'day'])
+_OFFSETS = {
+        'yesterday': -1 * (60 * 60 * 24)
+        }
 
 
 def getcurrentdate():
     """ Return the current date as a string that parsedate will understand.
 
-    The current day lasts until the hour reaches getconf('end_of_day'). e.g.,
-    If the day ends at 5 and it is 04:00 on 2012-04-08, this function will
-    return 2012-04-07."""
+    See getdate for a note on why a Unix time value of 2012-04-08 04:00 may
+    produce "2012-04-07".
+    """
+
+    return getdate(time())
+
+
+def getdate(when_secs):
+    """ Return the time given as a string that parsedate will understand.
+
+    Each day lasts until the hour reaches getconf('end_of_day'). e.g., If the
+    day ends at 5 and it is 04:00 on 2012-04-08, this function will return
+    2012-04-07. """
     end_of_day = getconf('end_of_day')
-    now = localtime(time() - (60 * 60 * end_of_day))
-    year = now.tm_year
-    month = now.tm_mon
-    day = now.tm_mday
+    when = localtime(when_secs - (60 * 60 * end_of_day))
+    year = when.tm_year
+    month = when.tm_mon
+    day = when.tm_mday
     return "%04d-%02d-%02d" % (year, month, day)
 
 
 def parsedate(date):
     """ Try to make sense of the user's date string. Raise a ValueError if we
     fail to do so. """
+    # If date gives a relative time in words (e.g., "yesterday"), apply the
+    # corresponding numerical offset to the current time and use that instead.
+    if date in _OFFSETS:
+        date = getdate(time() + _OFFSETS[date])
     pieces = date.split("-")
     if len(pieces) != 3:
         raise ValueError()
